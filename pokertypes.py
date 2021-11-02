c3c5d8ca012d185b0f9b5333509460c0fa93ded9
Florian Salaun <florian.salaun@nokia.com>
2021-11-02 13:52:07 +0100

    Added algorithm

--- pokertypes.py
+++ pokertypes.py
@@ -44,8 +44,20 @@ class Card:
         self.value = None
 
 
-def has_number_of_occurences(counter, occurence):
-    return len([x for x in counter.keys if counter[x] == occurence])
+def has_number_of(counter, number):
+    return len([x for x in counter.keys if counter[x] == number])
+
+
+def take_value(card):
+    return card.value
+
+
+def take_number_of(counter, number):
+    return [x for x in counter.keys if counter[x] == number][0]
+
+
+def take_pairs(counter):
+    return [x for x in counter.keys if counter[x] == 2]
 
 
 class Hand:
@@ -53,7 +65,31 @@ class Hand:
         self.cards = []
 
     def compute_value(self):
-        counter = Counter(self.cards)
-        number_of_pairs = has_number_of_occurences(counter, 2)
-        has_threes = has_number_of_occurences(counter, 3)
-        has_fours = has_number_of_occurences(counter, 4)
+        self.cards.sort(key=take_value)
+        values = [v.value for v in self.cards]
+        counter = Counter(values)
+        number_of_pairs = has_number_of(counter, 2)
+        has_threes = has_number_of(counter, 3)
+        has_fours = has_number_of(counter, 4)
+        is_color = len(Counter([v.color for v in self.cards])) == 1
+        is_straight = all(values[i + 1] == values[i] + 1 for i in range(len(values) - 1))
+        highest_card = self.cards[-1]
+        if is_straight and is_color:
+            if highest_card.value == Value.ACE:
+                return HandValues.ROYAL_FLUSH
+            return HandValues.STRAIGHT_FLUSH, highest_card
+        if has_fours:
+            return HandValues.FOUR_OF_A_KIND, take_number_of(counter, 4)
+        if has_threes and number_of_pairs == 1:
+            return HandValues.FULL_HOUSE, take_number_of(counter, 3)
+        if is_color:
+            return HandValues.FLUSH, highest_card
+        if is_straight:
+            return HandValues.STRAIGHT, highest_card
+        if has_threes:
+            return HandValues.THREE_OF_A_KIND, take_number_of(counter, 3)
+        if number_of_pairs == 2:
+            return HandValues.TWO_PAIR, take_pairs(counter), highest_card
+        if number_of_pairs == 1:
+            return HandValues.PAIR, take_pairs(counter), highest_card
+        return HandValues.HIGH_CARD, highest_card
